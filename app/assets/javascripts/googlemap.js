f327f13395da10aae17eeaf327863ed957bf36f4
daichi732 <ziima32@icloud.com>
2020-10-02 21:18:35 +0900

    ルート検索機能の関数定義

--- app/assets/javascripts/googlemap.js
+++ app/assets/javascripts/googlemap.js
@@ -1,6 +1,5 @@
 let map
 let geocoder
-let element
 let marker = [];
 let infoWindow = [];
 let markerData = gon.places; // コントローラーで定義したインスタンス変数を変数に代入
@@ -71,7 +70,7 @@ function initMap(){
       
       // 各地点の吹き出しを作成、jsではカラムが配列になるイメージ
       infoWindow[i] = new google.maps.InfoWindow({
-      content: `<a href='/places/${ id }'>${ markerData[i]['address'] }</a>`
+        content: `<a id: "btn-add" class: "btn btn-primary" onclick: "addPlace(markerData[i]['name'],markerData[i]['latitude'], markerData[i]['longitude'])">追加</a>`
       });
 
       // マーカーにクリックイベントを追加,初めはopen状態にする
@@ -103,4 +102,59 @@ function markerEvent(i) {
   marker[i].addListener('click', function () {
     infoWindow[i].open(map, marker[i]);
   });
+}
+
+// リストに追加する
+function addPlace(name, lat, lng){
+  var li = $('<li>', {
+      text: name,
+      "class": "list-group-item"
+  });
+  li.attr("data-lat", lat);
+  li.attr("data-lng", lng);
+  $('#route-list').append(li);
+}
+
+var rendererOptions = {
+  suppressMarkers : true
+}
+var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+var directionsService = new google.maps.DirectionsService();
+
+// 複数地点のルートを検索する
+function search() {
+  var points = $('#route-list li');
+
+  // 2地点以上のとき
+  if (points.length >= 2){
+      var origin; // 開始地点
+      var destination; // 終了地点
+      var waypoints = []; // 経由地点
+
+      // origin, destination, waypointsを設定する
+      for (var i = 0; i < points.length; i++) {
+          points[i] = new google.maps.LatLng($(points[i]).attr("data-lat"), $(points[i]).attr("data-lng"));
+          if (i == 0){
+              origin = points[i];
+          } else if (i == points.length-1){
+              destination = points[i];
+          } else {
+              waypoints.push({ location: points[i], stopover: true });
+          }
+      }
+      // リクエスト作成
+      var request = {
+          origin:      origin,
+          destination: destination,
+          waypoints: waypoints,
+          travelMode:  google.maps.TravelMode.DRIVING
+      };
+      // ルートサービスのリクエスト
+      directionsService.route(request, function(response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+              // 結果を表示する
+              directionsDisplay.setDirections(response);
+          }
+      });
+  }
 }
