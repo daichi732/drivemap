d001cdb184f35eca1adc144c18ee640830822813
daichi732 <ziima32@icloud.com>
2020-10-18 08:58:48 +0900

    FactoryBot userモデルスペック

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    name  { "testUser" }
+    email { "testemail" }
+    password { "testPassword" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,43 +2,50 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "名前、メールアドレス、パスワードがある場合有効であること" do
-    user = User.new(name: "testUser", email: "testemail", password: "testPassword")
-    expect(user).to be_valid
+    # user = User.new(name: "testUser", email: "testemail", password: "testPassword")
+    expect(FactoryBot.build(:user)).to be_valid
   end
 
   it "名前がない場合無効であること" do # to_not, validatesのコメントアウト
-    user = User.new(name: nil)
+    # user = User.new(name: nil)
+    user = FactoryBot.build(:user, name: nil)
     user.valid?
     expect(user.errors[:name]).to include("を入力してください")
   end
 
   it "名前が21文字の場合無効であること" do # to_not, validatesのコメントアウト
-    user = User.new(name: "a" * 21)
+    # user = User.new(name: "a" * 21)
+    user = FactoryBot.build(:user, name: "a" * 21)
     user.valid?
     expect(user.errors[:name]).to include("は20文字以内で入力してください")
   end
 
   it "メールアドレスがない場合無効であること" do
-    user = User.new(email: nil)
+    # user = User.new(email: nil)
+    user = FactoryBot.build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include("を入力してください")
   end
 
   it "重複したメールアドレスなら無効な状態であること" do
-    User.create(name: "testUser", email: "testemail", password: "testPassword")
-    user = User.new(email: "testemail")
+    # User.create(name: "testUser", email: "testemail", password: "testPassword")
+    FactoryBot.create(:user)
+    # user = User.new(email: "testemail")
+    user = FactoryBot.build(:user)
     user.valid?
     expect(user.errors[:email]).to include("はすでに存在します")
   end
 
   it "メールアドレスは小文字で保存されること" do
     email = "TestEmail"
-    user = User.create(name: "testUser", email: email, password: "testPassword")
+    # user = User.create(name: "testUser", email: email, password: "testPassword")
+    user = FactoryBot.create(:user, email: email)
     expect(user.email).to eq email.downcase
   end
 
   it "パスワードがない場合無効であること" do
-    user = User.new(password: nil)
+    # user = User.new(password: nil)
+    user = FactoryBot.build(:user, password: nil)
     user.valid?
     expect(user.errors[:password]).to include("を入力してください")
   end
