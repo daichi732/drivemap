a84bcc39368b9cafcf337f38e0160c46d0f35826
daichi732 <ziima32@icloud.com>
2020-10-18 12:28:00 +0900

    like invalid

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Like, type: :model do
     # idに値を入れてbuildする
     expect(FactoryBot.build_stubbed(:like)).to be_valid
   end
+
+  it "user_idがない場合無効であること" do
+    like = FactoryBot.build(:like, user_id: nil)
+    like.valid?
+    expect(like.errors[:user_id]).to include("を入力してください")
+  end
+
+  it "place_idがない場合無効であること" do
+    like = FactoryBot.build(:like, place_id: nil)
+    like.valid?
+    expect(like.errors[:place_id]).to include("を入力してください")
+  end
 end
