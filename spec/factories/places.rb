288bddd802f78fd923a369a197910be814a24adb
daichi732 <ziima32@icloud.com>
2020-10-18 09:56:58 +0900

    FactoryBot placeモデルスペック

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :place do
-    
+    name  { "testPlace" }
+    # description { "魅力的な場所です。" }
+    genre { 1 }
+    address { "東京タワー" }
+    # latitude { 35.6585805 }
+    # longitude { 139.7454329 }
+    association :user
   end
 end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Place, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "名称、種類、住所がある場合有効であること" do
+    place = FactoryBot.build(:place)
+    place.image = fixture_file_upload("/files/test_image.png")
+    expect(place).to be_valid
+  end
 end
