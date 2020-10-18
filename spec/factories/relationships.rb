b42a5681b721548971fdf7d18a1f7dbd95a78d9c
daichi732 <ziima32@icloud.com>
2020-10-18 21:32:43 +0900

    aliasの適用

--- spec/factories/relationships.rb
+++ spec/factories/relationships.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :relationship do
-    association :follower, factory: :user
-    association :followed, factory: :user
+    association :follower
+    association :followed
   end
 end
