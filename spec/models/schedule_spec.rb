cf882dd09326746766aafc084f8a437d7c97be70
daichi732 <ziima32@icloud.com>
2020-10-19 12:21:05 +0900

    models schedule_spec letの利用

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,24 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Schedule, type: :model do
+  let(:schedule) { FactoryBot.build(:schedule) }
+
   it "user_id, place_id, dateがある場合有効であること" do
     expect(FactoryBot.build_stubbed(:schedule)).to be_valid
   end
 
   it "user_idがない場合無効であること" do
-    schedule = FactoryBot.build(:schedule, user_id: nil)
+    schedule.user_id = nil
     schedule.valid?
     expect(schedule.errors[:user_id]).to include("を入力してください")
   end
 
   it "place_idがない場合無効であること" do
-    schedule = FactoryBot.build(:schedule, place_id: nil)
+    schedule.place_id = nil
     schedule.valid?
     expect(schedule.errors[:place_id]).to include("を入力してください")
   end
 
   it "dateがない場合無効であること" do
-    schedule = FactoryBot.build(:schedule, date: nil)
+    schedule.date = nil
     schedule.valid?
     expect(schedule.errors[:date]).to include("を入力してください")
   end
