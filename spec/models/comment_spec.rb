51967a229c72f2e7b3a790f15e3c830a15de3701
daichi732 <ziima32@icloud.com>
2020-10-19 12:12:38 +0900

    models comment_spec letの利用

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,30 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  let(:comment) { FactoryBot.build(:comment) }
+
   it "user_id, place_id, contentがある場合有効であること" do
     expect(FactoryBot.build_stubbed(:comment)).to be_valid
-  end
+  endx
 
   it "user_idがない場合無効であること" do
-    comment = FactoryBot.build(:comment, user_id: nil)
+    comment.user_id = nil
     comment.valid?
     expect(comment.errors[:user_id]).to include("を入力してください")
   end
 
   it "place_idがない場合無効であること" do
-    comment = FactoryBot.build(:comment, place_id: nil)
+    comment.place_id = nil
     comment.valid?
     expect(comment.errors[:place_id]).to include("を入力してください")
   end
 
   it "contentがない場合無効であること" do
-    comment = FactoryBot.build(:comment, content: nil)
+    comment.content = nil
     comment.valid?
     expect(comment.errors[:content]).to include("を入力してください")
   end
 
   it "contentが51文字の場合無効であること" do
-    comment = FactoryBot.build(:comment, content: "a" * 51)
+    comment.content = "a" * 51
     comment.valid?
     expect(comment.errors[:content]).to include("は50文字以内で入力してください")
   end
