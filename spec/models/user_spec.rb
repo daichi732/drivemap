ec0556119795736984019b8db363f823aa4c626b
daichi732 <ziima32@icloud.com>
2020-10-19 18:01:01 +0900

    models user_spec letを一つ上階層でDRY

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -66,86 +66,22 @@ RSpec.describe User, type: :model do
         expect(user.errors[:avatar]).to include("にはjpegまたはgifまたはpngファイルを添付してください")
       end
     end
-
-    context "ロジック" do
-      context "フォロー機能" do
-        it "フォローとアンフォローが正常に動作すること" do
-          expect(user.following?(other_user)).to be_falsey
-          user.follow(other_user)
-          expect(user.following?(other_user)).to be_truthy
-          user.unfollow(other_user)
-          expect(user.following?(other_user)).to be_falsey
-        end
-      end
-  
-      context "own?(object)メソッド" do
-        it "Userモデルインスタンスが自分のplaceであるか確認すること" do
-          # 本来 user = FactoryBot.create(:user)アソシエーションあるからいらない！！
-          
-          # user = FactoryBot.create(:user)
-          # other_user = FactoryBot.create(:user)
-    
-          # アソシエーションでuserインスタンスも作成されることからcreateにしたいけどimageで引っかかってしまうから
-          user_place = FactoryBot.build(:place, user_id: user.id)
-          user_place.image = fixture_file_upload("/files/test_image.png")
-          user_place.save
-    
-          other_user_place = FactoryBot.build(:place, user_id: other_user.id)
-          other_user_place.image = fixture_file_upload("/files/test_image.png")
-          other_user_place.save
-    
-          expect(user.own?(user_place)).to be_truthy
-          expect(user.own?(other_user_place)).to be_falsey
-        end
-    
-        it "Userモデルインスタンスが自分のcommentであるか確認すること" do
-          # user = FactoryBot.create(:user)
-          # other_user = FactoryBot.create(:user)
-    
-          common_place = FactoryBot.build(:place)
-          common_place.image = fixture_file_upload("/files/test_image.png")
-          common_place.save
-    
-          user_comment = FactoryBot.create(:comment, user_id: user.id, place_id: common_place.id)
-    
-          other_user_comment = FactoryBot.create(:comment, user_id: other_user.id, place_id: common_place.id)
-    
-          expect(user.own?(user_comment)).to be_truthy
-          expect(user.own?(other_user_comment)).to be_falsey
-        end
-    
-        it "Userモデルインスタンスが自分のscheduleであるか確認すること" do
-          user = FactoryBot.create(:user)
-          other_user = FactoryBot.create(:user)
-    
-          common_place = FactoryBot.build(:place)
-          common_place.image = fixture_file_upload("/files/test_image.png")
-          common_place.save
-    
-          user_schedule = FactoryBot.create(:schedule, user_id: user.id, place_id: common_place.id)
-    
-          other_user_schedule = FactoryBot.create(:schedule, user_id: other_user.id, place_id: common_place.id)
-    
-          expect(user.own?(user_schedule)).to be_truthy
-          expect(user.own?(other_user_schedule)).to be_falsey
-        end
-      end
-    end
   end
-
+  
+  
   describe "アソシエーションのテスト" do
     let(:association) do
       # Userクラスと引数のクラスの関連を返す
       described_class.reflect_on_association(target)
     end
-
+    
     context 'Placeモデルとの関連' do
       let(:target) { :places }
-
+      
       it '1対多である' do
         expect(association.macro).to eq :has_many
       end
-
+      
       it "userを削除すると、placeも削除される" do
         place = FactoryBot.build(:place, user_id: user.id)
         place.image = fixture_file_upload("/files/test_image.png")
@@ -153,14 +89,14 @@ RSpec.describe User, type: :model do
         expect{ user.destroy }.to change{ Place.count }.by(-1)
       end
     end
-
+    
     context 'Likeモデルとの関連' do
       let(:target) { :likes }
-
+      
       it '1対多である' do
         expect(association.macro).to eq :has_many
       end
-
+      
       it "userを削除すると、likeも削除される" do
         place = FactoryBot.build(:place, user_id: user.id)
         place.image = fixture_file_upload("/files/test_image.png")
@@ -170,14 +106,14 @@ RSpec.describe User, type: :model do
         expect{ user.destroy }.to change{ Like.count }.by(-1)
       end
     end
-
+    
     context 'Commentモデルとの関連' do
       let(:target) { :comments }
-
+      
       it '1対多である' do
         expect(association.macro).to eq :has_many
       end
-
+      
       it "userを削除すると、commentも削除される" do
         place = FactoryBot.build(:place, user_id: user.id)
         place.image = fixture_file_upload("/files/test_image.png")
@@ -187,14 +123,14 @@ RSpec.describe User, type: :model do
         expect{ user.destroy }.to change{ Comment.count }.by(-1)
       end
     end
-
+    
     context 'Scheduleモデルとの関連' do
       let(:target) { :schedules }
-
+      
       it '1対多である' do
         expect(association.macro).to eq :has_many
       end
-
+      
       it "userを削除すると、scheduleも削除される" do
         place = FactoryBot.build(:place, user_id: user.id)
         place.image = fixture_file_upload("/files/test_image.png")
@@ -204,26 +140,26 @@ RSpec.describe User, type: :model do
         expect{ user.destroy }.to change{ Schedule.count }.by(-1)
       end
     end
-
+    
     context 'Relationshipモデル(active_relationships)モデルとの関連' do
       let(:target) { :active_relationships }
-
+      
       it '1対多である' do
         expect(association.macro).to eq :has_many
       end
       it '結合モデルのクラスはRelationship' do
         expect(association.class_name).to eq 'Relationship'
       end
-
+      
       it "userを削除すると、relationshipsも削除される" do
         user.follow(other_user)
         expect{ user.destroy }.to change{ Relationship.count }.by(-1)
       end
     end
-
+    
     context 'Relationshipモデル(passive_relationships)モデルとの関連' do
       let(:target) { :passive_relationships }
-
+      
       it '1対多である' do
         expect(association.macro).to eq :has_many
       end
@@ -231,10 +167,10 @@ RSpec.describe User, type: :model do
         expect(association.class_name).to eq 'Relationship'
       end
     end
-
+    
     context 'フォローしているユーザーとの関連' do
       let(:target) { :following }
-
+      
       it '1対多である' do
         expect(association.macro).to eq :has_many
       end
@@ -242,10 +178,10 @@ RSpec.describe User, type: :model do
         expect(association.class_name).to eq 'User'
       end
     end
-
+    
     context 'フォロワーとの関連' do
       let(:target) { :followers }
-
+      
       it '1対多である' do
         expect(association.macro).to eq :has_many
       end
@@ -253,7 +189,70 @@ RSpec.describe User, type: :model do
         expect(association.class_name).to eq 'User'
       end
     end
-
-    
+  end
+  
+  describe "ロジックのテスト" do
+    context "フォロー機能" do
+      it "フォローとアンフォローが正常に動作すること" do
+        expect(user.following?(other_user)).to be_falsey
+        user.follow(other_user)
+        expect(user.following?(other_user)).to be_truthy
+        user.unfollow(other_user)
+        expect(user.following?(other_user)).to be_falsey
+      end
+    end
+  
+    context "own?(object)メソッド" do
+      it "Userモデルインスタンスが自分のplaceであるか確認すること" do
+        # 本来 user = FactoryBot.create(:user)アソシエーションあるからいらない！！
+        
+        # user = FactoryBot.create(:user)
+        # other_user = FactoryBot.create(:user)
+  
+        # アソシエーションでuserインスタンスも作成されることからcreateにしたいけどimageで引っかかってしまうから
+        user_place = FactoryBot.build(:place, user_id: user.id)
+        user_place.image = fixture_file_upload("/files/test_image.png")
+        user_place.save
+  
+        other_user_place = FactoryBot.build(:place, user_id: other_user.id)
+        other_user_place.image = fixture_file_upload("/files/test_image.png")
+        other_user_place.save
+  
+        expect(user.own?(user_place)).to be_truthy
+        expect(user.own?(other_user_place)).to be_falsey
+      end
+  
+      it "Userモデルインスタンスが自分のcommentであるか確認すること" do
+        # user = FactoryBot.create(:user)
+        # other_user = FactoryBot.create(:user)
+  
+        common_place = FactoryBot.build(:place)
+        common_place.image = fixture_file_upload("/files/test_image.png")
+        common_place.save
+  
+        user_comment = FactoryBot.create(:comment, user_id: user.id, place_id: common_place.id)
+  
+        other_user_comment = FactoryBot.create(:comment, user_id: other_user.id, place_id: common_place.id)
+  
+        expect(user.own?(user_comment)).to be_truthy
+        expect(user.own?(other_user_comment)).to be_falsey
+      end
+  
+      it "Userモデルインスタンスが自分のscheduleであるか確認すること" do
+        user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+  
+        common_place = FactoryBot.build(:place)
+        common_place.image = fixture_file_upload("/files/test_image.png")
+        common_place.save
+  
+        user_schedule = FactoryBot.create(:schedule, user_id: user.id, place_id: common_place.id)
+  
+        other_user_schedule = FactoryBot.create(:schedule, user_id: other_user.id, place_id: common_place.id)
+  
+        expect(user.own?(user_schedule)).to be_truthy
+        expect(user.own?(other_user_schedule)).to be_falsey
+      end
+    end
   end
 end
