0866be61dc9a99ca427568861ae473eaa790509c
daichi732 <ziima32@icloud.com>
2020-10-21 18:55:30 +0900

    system place_spec 場所の編集

--- spec/system/place_spec.rb
+++ spec/system/place_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 RSpec.describe Place, type: :system do
-  let(:user) { FactoryBot.create(:user) }
-  # let(:place) { FactoryBot.create(:place) }
+  # let(:user) { FactoryBot.create(:user) }
+  let(:place) { FactoryBot.create(:place) }
+
+  before do
+    visit login_path
+    fill_in "session[email]", with: "#{ place.user.email }" # 作成されたplaceのuserでログインする
+    fill_in "session[password]", with: "password"
+    click_on "ログインする"
+  end
   
   describe "場所の登録ページ" do
     before do
-      visit login_path
-      fill_in "session[email]", with: "#{ user.email }"
-      fill_in "session[password]", with: "password"
-      click_on "ログインする"
       visit new_place_path
     end
 
@@ -58,6 +61,30 @@ RSpec.describe Place, type: :system do
         expect(page).to have_content "住所を入力してください"
       end
     end
+  end
 
+  describe "場所の編集ページ" do
+    before do
+      visit edit_place_path(place)
+    end
+
+    context "ページレイアウト" do
+      it "「場所を編集する」の文字列が存在することを確認" do
+        expect(page).to have_content '場所を編集する'
+      end
+    end
+
+    # 違うuserでログインして権限がありませんのテストも
+    context "フォームの入力が正しい時" do
+      it "編集に成功し、フラッシュメッセージを表示する" do
+        fill_in "place[name]", with: "edit_place"
+        find('label[for=place_genre_food]').click 
+        fill_in "place[address]", with: "箱根ガラスの森美術館"
+        click_on "更新する"
+        expect(page).to have_content "「edit_place」を更新しました。"
+        expect(page).to have_content "edit_place"
+        # expect(current_path).to eq place_path( place.id )
+      end
+    end
   end
 end
